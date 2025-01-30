6b0b2d307dc2c19b7a7f33ca01ef8f31d0c0b77f
Jon Staab <shtaab@gmail.com>
2025-01-29 17:32:46 -0800

    Rough out subscription

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use axum::{
     http::StatusCode,
     response::IntoResponse,
 };
+use std::time::{Duration};
+use nostr_sdk::{Client, Filter, Keys, Timestamp};
+use nostr_sdk::event::kind::{EPHEMERAL_RANGE};
+use nostr_sdk::async_utility::futures_util::StreamExt;
 use serde::Deserialize;
 use serde_json::json;
 use lmdb;
@@ -60,8 +64,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .open(FilePath::new("data/db"))?);
 
     // Make sure our database exists and get a handle to it
-    env.create_db(None, lmdb::DatabaseFlags::empty())?;
-    let db = env.open_db(None)?;
+    let db = env.create_db(None, lmdb::DatabaseFlags::empty())?;
 
     let state = Arc::new(AppState {
         env,
@@ -71,11 +74,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Build our application with a route
     let app = Router::new()
         .route("/notary/:key", get(get_timestamp))
-        .with_state(state);
+        .with_state(state.clone());
 
     // Run it on localhost:3000
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
     println!("Server running on http://127.0.0.1:3000");
+
+    // Set up a nostr client and listen for events to notarize
+    let keys: Keys = Keys::generate();
+    let client = Client::new(keys);
+    client.add_relay("wss://relay.damus.io").await?;
+    client.add_relay("wss://nos.lol").await?;
+    client.connect().await;
+
+    let filter = Filter::new()
+        .since(Timestamp::now() - Duration::from_secs(30));
+
+    let mut stream = client.stream_events(vec![filter], Duration::MAX).await?;
+
+    while let Some(event) = stream.next().await {
+        if EPHEMERAL_RANGE.contains(&event.kind.as_u16()) {
+            continue
+        }
+
+        println!("Received event: {:?}", event);
+
+        let mut txn = state.env.begin_rw_txn()?;
+        let timestamp = Timestamp::now();
+        let secs = timestamp.as_u64().to_le_bytes();
+
+        txn.put(state.db, &event.id, &secs, lmdb::WriteFlags::empty())?;
+    }
 
     Ok(axum::serve(listener, app).await?)
 }
